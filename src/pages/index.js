b05bffa0c97013d6e7bfda5cf1b738dde1a43808
Тёма <tema@MacBook-Pro-Tema.local>
2018-09-01 18:56:09 +0100

    CSS added

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,14 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>This is it</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Another link</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>This is it</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <Link to="/page-2/">Another link</Link>
+      </div>
+    </div>
   </div>
 )
 
