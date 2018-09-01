5129557ce7fc78035d0d236ad289bc3781ad5b25
Тёма <tema@MacBook-Pro-Tema.local>
2018-09-01 12:05:46 +0100

    Check

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>This is it</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Another link</Link>
+  </div>
+)
+
+export default IndexPage
